c306d52f50b15407917ed2076d6be5b3c6d57f83
Marc-André Appel <marc-andre@hybride-conseil.fr>
2020-11-22 23:52:40 +0100

    feat(user:edit) add the new command for editing user data

--- src/ArtisanUsers.php
+++ src/ArtisanUsers.php
@@ -16,16 +16,19 @@ class ArtisanUsers
 {
 
     /**
-     * @var string $user
+     * @var string $model
      */
-    private string $user;
+    private string $model;
+
+    /** @var Model $user */
+    public Model $user;
 
     /**
      * ArtisanUsers constructor.
      */
     public function __construct()
     {
-        $this->user = config('artisan_users.use_model');
+        $this->model = config('artisan_users.use_model');
     }
 
     /**
@@ -38,7 +41,7 @@ class ArtisanUsers
         /**
          * @var Model $user
          */
-        $user = new $this->user();
+        $user = new $this->model();
 
         $user->name     = $values->get('name');
         $user->email    = $values->get('email');
@@ -52,6 +55,57 @@ class ArtisanUsers
     }
 
     /**
+     * @param  string  $email
+     *
+     * @return ArtisanUsers
+     */
+    public function updateUser(string $email): self
+    {
+        $this->user = $this->model::where('email', $email)->first();
+
+        return $this;
+    }
+
+    /**
+     * Updates the email address
+     *
+     * @param  string  $email
+     *
+     * @return bool
+     */
+    public function setEmail(string $email): bool
+    {
+        $this->user->email = $email;
+        return $this->user->save();
+    }
+
+    /**
+     * Updates the name associated to the user
+     *
+     * @param  string  $name
+     *
+     * @return bool
+     */
+    public function setName(string $name)
+    {
+        $this->user->name = $name;
+        return $this->user->save();
+    }
+
+    /**
+     * Updates the password
+     *
+     * @param  string  $password
+     *
+     * @return bool
+     */
+    public function setPassword(string $password)
+    {
+        $this->user->password = Hash::make($password);
+        return $this->user->save();
+    }
+
+    /**
      * Verify the existence of a record with the given email address
      *
      * @param  string  $email
@@ -61,7 +115,7 @@ class ArtisanUsers
     public function userExists(string $email): bool
     {
         try {
-            $this->user::where('email', $email)->firstOrFail();
+            $this->model::where('email', $email)->firstOrFail();
 
             return true;
         } catch (\Exception $exception) {
--- src/Console/Commands/UserEdit.php
+++ src/Console/Commands/UserEdit.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 namespace MarcAndreAppel\ArtisanUsers\Console\Commands;
 
 use Illuminate\Console\Command;
+use MarcAndreAppel\ArtisanUsers\Facades\ArtisanUsers;
 
+/**
+ * Class UserEdit
+ *
+ * @package MarcAndreAppel\ArtisanUsers\Console\Commands
+ */
 class UserEdit extends Command
 {
 
@@ -12,7 +18,7 @@ class UserEdit extends Command
     protected $signature = 'user:edit';
 
     /** @var string */
-    protected $description = "Modify an user";
+    protected $description = "Edit a user";
 
     /** @var bool $withRoles */
     private bool $withRoles;
@@ -29,5 +35,42 @@ class UserEdit extends Command
     public function handle()
     {
         $email = $this->ask("Users email address");
+
+        if (ArtisanUsers::userExists($email) === false) {
+            $this->warn("User doesn't exist. use <info>'artisan user:add'</info> instead.");
+        } else {
+            $choice = $this->choice("Choose from the following to edit",
+                ['Email', 'Name', 'Password', 'Cancel']
+            );
+
+            switch ($choice) {
+                case 'cancel':
+                    $this->info("As you wish.");
+                    break;
+                case 'email':
+                    $newEmail = $this->ask("New email address");
+                    if (ArtisanUsers::userExists($newEmail) === false) {
+                        ArtisanUsers::updateUser($email)->setEmail($newEmail);
+                        $this->info("Email address was modified.");
+                    } else {
+                        $this->error("Email address already on record!");
+                    }
+                    break;
+                case 'name':
+                    $newName = $this->ask("New first and last name");
+                    ArtisanUsers::updateUser($email)->setName($newName);
+                    $this->info("First and last name were modified.");
+                    break;
+                /**
+                 * @fixme Add verification to the password question
+                 */
+                case 'password':
+                    $newPassword = $this->ask("New password");
+                    ArtisanUsers::updateUser($email)->setPassword($newPassword);
+                    $this->info("Password was modified.");
+                    break;
+            }
+        }
     }
+
 }
